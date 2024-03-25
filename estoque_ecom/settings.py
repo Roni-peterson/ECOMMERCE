ad0fbf57cdb5607eb9ba9701b36ad29e447b623b
Ronipeterso Gonçalves <jaum15goncalves@outlook.com>
2024-03-25 13:21:49 -0300

    resolvendo problemas

--- estoque_ecom/settings.py
+++ estoque_ecom/settings.py
@@ -132,7 +132,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR,"statics")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,"static")
+]
 
 # Define o diretório onde os arquivos de mídia serão armazenados
 # Define a URL base para servir arquivos de mídia
